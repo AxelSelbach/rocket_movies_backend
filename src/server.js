d4b937bc5aa92c140f53c126fa3c21b6389e8bad
axel <axelselbach90@gmail.com>
2023-10-18 19:51:52 -0300

    body params

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World!')
+app.use(express.json())
+
+app.post('/users', (request, response) => {
+  const { name, email, password } = request.body;
+
+  response.json({ name, email, password })
 })
 
-const PORT = 3333
+const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
